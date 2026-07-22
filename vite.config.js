7edb173c270811fdcc0e0a514b9b96a4d0a40ae5
ShaikRehman786 <shaikrehman78609@gmail.com>
2026-07-22 09:55:18 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-
-// Load .env variables
-dotenv.config();
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
@@ -11,11 +7,11 @@ export default defineConfig({
     host: true,
     allowedHosts: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: process.env.VITE_API_URL || "http://localhost:5000",
         changeOrigin: true,
-        ws: true
-      }
-    }
-  }
-})
+        ws: true,
+      },
+    },
+  },
+});
